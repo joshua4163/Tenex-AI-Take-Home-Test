f4a82efebcb50ff41b26805021ba764f2033d9e7
Josh <joshuakrishna21@gmail.com>
2026-04-06 16:31:03 -0500

    Updated -Not for  Cloud deployment

--- backend/parser.py
+++ backend/parser.py
@@ -2,13 +2,8 @@ import pandas as pd
 import io
 
 def parse_zscaler_logs(content: str):
-    # CSV format lo read chesthunnam (Zscaler NSS standard)
     df = pd.read_csv(io.StringIO(content))
     
-    # Manaki kavalsina columns ni filter/rename cheddam
-    # Zscaler fields: %s{time}, %s{login}, %s{url}, %s{action}, %d{totalsize}, %d{riskscore}
-    
-    # Dashboard lo chupinchadaniki mapping
     mapping = {
         'time': 'timestamp',
         'login': 'user',
